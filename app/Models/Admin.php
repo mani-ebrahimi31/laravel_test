6c7625ec4211fbd31f3abf4a73fc0c1434916d5e
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-10-15 19:14:45 +0330

    updated Admin mode;l

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
+
+    protected $table = 'admins';
+
+    protected $fillable = [
+        'name',
+        'password'
+    ];
 }
